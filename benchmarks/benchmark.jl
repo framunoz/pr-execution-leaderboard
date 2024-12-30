b8e6fa7eed5f2a17492f57b48f7a38ce41446ee0
Stockless <mar.stockle@gmail.com>
2024-12-30 14:57:45 -0300

    group_results

--- benchmarks/benchmark.jl
+++ benchmarks/benchmark.jl
@@ -2,8 +2,11 @@ using BenchmarkTools
 push!(LOAD_PATH, "src")
 using MyModule  # Import your module
 
-# Benchmark the expensive_computation function
-results = @benchmark expensive_computation(20)  # Benchmark for n = 30
-
+suite = BenchmarkGroup()
+suite["ec"] = BenchmarkGroup(["tag1","tag2"])
+suite["ec"][20] = @benchmarkable expensive_computation(20)
+suite["ec"][30] = @benchmarkable expensive_computation(30)
+tune!(suite)
+results = run(suite,verbose = true)
 # Save the benchmark results to a JSON file
 BenchmarkTools.save("benchmarks/benchmark_results.json", median(results))
