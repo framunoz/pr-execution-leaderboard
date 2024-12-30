76236945ba8a6171c203a148b56b6ad47e63715f
Stockless <mar.stockle@gmail.com>
2024-12-30 14:47:02 -0300

    saving median results

--- benchmarks/benchmark.jl
+++ benchmarks/benchmark.jl
@@ -6,4 +6,4 @@ using MyModule  # Import your module
 results = @benchmark expensive_computation(20)  # Benchmark for n = 30
 
 # Save the benchmark results to a JSON file
-BenchmarkTools.save("benchmarks/benchmark_results.json", results)
+BenchmarkTools.save("benchmarks/benchmark_results.json", median(results))
