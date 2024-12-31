feef9cf8b4de4b7a761959c1e92b4876942318be
Stockless <mar.stockle@gmail.com>
2024-12-31 19:01:48 -0300

    fix benchmark

--- benchmarks/benchmark.jl
+++ benchmarks/benchmark.jl
@@ -7,7 +7,7 @@ github_username = get(ENV, "GITHUB_ACTOR", "UNKNOWN")
 
 suite = BenchmarkGroup()
 suite["function"] = BenchmarkGroup(["USERNAME"])  
-suite["function1"][github_username] = @benchmarkable expensive_computation()
+suite["function1"][github_username] = @benchmarkable expensive_computation(30000)
 tune!(suite)
 results = run(suite, verbose=true)
 
