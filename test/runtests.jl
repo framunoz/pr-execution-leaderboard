b6d262dd97304369cd9bc1de082a81ff0ceba4a5
Carlos Castillo Passi <cncastillo@uc.cl>
2025-01-02 01:17:21 -0300

    Passing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test
 using MyPkg
 
-expected_result    = 34 - 1 #solve(..., Theoretical())
+expected_result    = 34 #solve(..., Theoretical())
 numerical_solution = expensive_computation(10) #solve(..., ForwardEuler())
 
 @test numerical_solution ≈ expected_result
