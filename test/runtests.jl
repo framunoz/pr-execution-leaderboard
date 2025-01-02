4665e8ac3c9dc211e4692d3fcb7bb8f8971eaa04
Stockless <mar.stockle@gmail.com>
2025-01-02 18:15:42 -0300

    2 functions test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,12 @@
 using Test
 using MyPkg
 
-expected_result    = 34 #solve(..., Theoretical())
-numerical_solution = expensive_computation(10) #solve(..., ForwardEuler())
+expected_result1    = 34 #solve(..., Theoretical())
+numerical_solution1 = expensive_computation(10) #solve(..., ForwardEuler())
+expected_result2    = 4 #solve(..., Theoretical())
+numerical_solution2 = fast_function() #solve(..., ForwardEuler())
 
-@test numerical_solution ≈ expected_result
+@test numerical_solution1 ≈ expected_result1
+
+
+@test numerical_solution2 ≈ expected_result2
