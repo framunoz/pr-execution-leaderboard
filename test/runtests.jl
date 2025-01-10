01c9f523f9e410fbf6e8f03c18bb9e5348b44a28
Carlos Castillo Passi <cncastillo@uc.cl>
2025-01-10 17:41:10 -0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ m0   = [1.0, 0.0, 0.0]
 Δt   = 0.001
 tmax = 3.0
 
-expected_result    = solver(m0, Δt, tmax, Theoretical())
-numerical_solution = solver(m0, Δt, tmax, ForwardEuler())
+expected_result    = solve(m0, Δt, tmax, Theoretical())
+numerical_solution = solve(m0, Δt, tmax, ForwardEuler())
 
 @test numerical_solution ≈ expected_result
