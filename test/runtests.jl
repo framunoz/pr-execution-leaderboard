03b50162b09f10a7e528a19690f831c4869769e4
Francisco Muñoz <femunoz@dim.uchile.cl>
2025-01-10 23:28:30 -0300

    test: debug test: using abs norm with fixed threshold

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,11 @@ using MyPkg
 
 # Inputs
 m0   = [1.0, 0.0, 0.0]
-Δt   = 0.001
+Δt   = 1e-5
 tmax = 3.0
 
 expected_result    = solve(m0, Δt, tmax, Theoretical())
 numerical_solution = solve(m0, Δt, tmax, ForwardEuler())
+maxError           = maximum(abs.(numerical_solution .- expected_result))
 
-@test numerical_solution ≈ expected_result
+@test maxError < 1e-3
